435d3aed1ccf1dc3272c6946037612493924a76a
azoght <angelina@zoght.com>
2020-07-06 18:33:58 -0700

    added instantRunoff and bordaCount functions, as well as a function within the voteCount class that makes a count register

--- main.py
+++ main.py
@@ -20,6 +20,20 @@ def isEqualToAll(l1: [],l2: []) -> bool:
             return True
     return False
 
+def hasMajority(b: Ballot) -> bool:
+    majThreshold = 0
+    for v in b.votes:
+        if b.votes[v] != -1:
+            majThreshold +=1
+    majThreshold = majThreshold // 2
+    biggest = 0
+    for v in b.votes:
+        if b.votes[v] > b.votes[biggest]:
+            biggest = v
+    if b.votes[biggest] >= majThreshold:
+        return True
+    return False
+
 class BallotRegistry:
 
     def __init__(self):
@@ -40,11 +54,15 @@ class VoteCount:
     def __init__(self,br: BallotRegistry):
         self.registry = br
 
-    def plurality(self):
+    def __makecountregister__(self) -> Ballot:
         vee = []
         for c in range(len(self.registry.br[0].candidates)):
             vee.append(0)
-        count_register = Ballot(c=self.registry.br[0].candidates,v=vee)
+        count_register = Ballot(c=self.registry.br[0].candidates, v=vee)
+        return count_register
+
+    def plurality(self):
+        count_register = self.__makecountregister__()
         for b in self.registry.br:
             for r in b:
                 if b.votes[r] == 1:
@@ -59,10 +77,44 @@ class VoteCount:
         print("Winner is",count_register.candidates[winner])
 
     def instantrunoff(self):
-        pass
+        count_register = self.__makecountregister__()
+        for o in range(1, len(count_register.candidates) - 1):
+            for b in self.registry.br:
+                for r in b:
+                    if b.votes[r] == o and count_register.candidates[r] != -1:
+                        count_register.votes[r] += 1
+            least = 0
+            for c in count_register.votes:
+                if count_register.votes[c] < count_register.votes[least] and count_register.votes[c] != -1:
+                    least = c
+
+            if o > 1 and hasMajority(count_register):
+                break
+        winner = 0
+        for c in count_register.votes:
+            if count_register.votes[c] > count_register.votes[winner]:
+                winner = c
+
+        print("Winner is",count_register.candidates[winner])
 
     def bordacount(self):
-        pass
+        count_register = self.__makecountregister__()
+        bordalist = []
+        for i in reversed(range(len(count_register.votes) - 1)):
+            bordalist.append(i)
+
+        for o in range(1,len(count_register.candidates)):
+            for b in self.registry.br:
+                for r in b:
+                    if b[r] == o:
+                        count_register.votes[r] += bordalist[o - 1]
+
+        winner = 0
+        for c in count_register.votes:
+            if count_register.votes[c] > count_register.votes[winner]:
+                winner = c
+
+        print("Winner is", count_register.candidates[winner])
 
     def condorcet(self):
         pass
