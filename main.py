93c233a44ce0c193e03d0f75468e0cadad9c4ed0
azoght <angelina@zoght.com>
2020-07-26 15:53:45 -0700

    testing begins

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ class Ballot:
 def isEqualToAll(l1: [],l2: []) -> bool:
     if len(l1) == len(l2):
         count = 0
-        for i in l1:
-            if l1(i) == l2(i):
+        for i in range(len(l1)):
+            if l1[i] == l2[i]:
                 count += 1
         if count == len(l1):
             return True
@@ -23,12 +23,12 @@ def isEqualToAll(l1: [],l2: []) -> bool:
 
 def hasMajority(b: Ballot) -> bool:
     majThreshold = 0
-    for v in b.votes:
+    for v in range(len(b.votes)):
         if b.votes[v] != -1:
             majThreshold +=1
     majThreshold = majThreshold // 2
     biggest = 0
-    for v in b.votes:
+    for v in range(len(b.votes)):
         if b.votes[v] > b.votes[biggest]:
             biggest = v
     if b.votes[biggest] >= majThreshold:
@@ -36,15 +36,9 @@ def hasMajority(b: Ballot) -> bool:
     return False
 
 def containsBoth(l: [],a1,a2) -> bool:
-    count1 = 0
-    count2 = 0
-    for i in l:
-        if l[i] == a1:
-            count1 += 1
-        if l[i] == a2:
-            count2 += 1
-    if count1 > 0 and count2 > 0:
-        return True
+    for i in range(len(l)):
+        if l[i] == [a1,a2] or l[i] == [a2,a1]:
+            return True
     return False
 
 def isHigherThan(b: Ballot, b1: int, b2: int) -> bool:
@@ -81,13 +75,13 @@ class VoteCount:
     def plurality(self):
         count_register = self.__makecountregister__()
         for b in self.registry.br:
-            for r in b:
+            for r in range(len(b.votes)):
                 if b.votes[r] == 1:
                     count_register.votes[r] += 1
 
         winner = 0
 
-        for c in count_register.votes:
+        for c in range(len(count_register.votes)):
             if count_register.votes[c] > count_register.votes[winner]:
                 winner = c
 
@@ -95,20 +89,25 @@ class VoteCount:
 
     def instantrunoff(self):
         count_register = self.__makecountregister__()
-        for o in range(1, len(count_register.candidates) - 1):
+        for o in range(1, len(count_register.candidates)):
             for b in self.registry.br:
-                for r in b:
+                for r in range(len(b.votes)):
                     if b.votes[r] == o and count_register.candidates[r] != -1:
                         count_register.votes[r] += 1
+                        break
             least = 0
-            for c in count_register.votes:
-                if count_register.votes[c] < count_register.votes[least] and count_register.votes[c] != -1:
-                    least = c
 
             if o > 1 and hasMajority(count_register):
                 break
+
+            for c in range(len(count_register.votes)):
+                if count_register.votes[c] < count_register.votes[least] and count_register.votes[c] != -1:
+                    least = c
+
+            count_register.votes[least] = -1
+
         winner = 0
-        for c in count_register.votes:
+        for c in range(len(count_register.votes)):
             if count_register.votes[c] > count_register.votes[winner]:
                 winner = c
 
@@ -122,70 +121,122 @@ class VoteCount:
 
         for o in range(1,len(count_register.candidates)):
             for b in self.registry.br:
-                for r in b:
-                    if b[r] == o:
+                for r in range(len(b.votes)):
+                    if b.votes[r] == o:
                         count_register.votes[r] += bordalist[o - 1]
 
         winner = 0
-        for c in count_register.votes:
+        for c in range(len(count_register.votes)):
             if count_register.votes[c] > count_register.votes[winner]:
-                winner = count_register.candidates[winner]
+                winner = c
 
-        print("Winner is", winner)
+        print("Winner is", count_register.candidates[winner])
 
     def condorcet(self):
         count_register = self.__makecountregister__()
         roundList = []
         r = []
         fillingList = True
-        co = False
         lengthofRoundList = 0.5 * (len(count_register.candidates) ** 2) - 0.5 * len(count_register.candidates)
         while fillingList:
-            r1 = random.uniform(0,len(count_register.candidates) - 1)
-            r2 = random.uniform(0,len(count_register.candidates) - 1)
+            r1 = random.randint(0,len(count_register.candidates) - 1)
+            r2 = random.randint(0,len(count_register.candidates) - 1)
             if r1 != r2:
-                for ro in roundList:
-                    if containsBoth(ro, r1, r2):
-                        co = True
-                        break
-                if co == True:
-                    continue
-                r.append(r1)
-                r.append(r2)
-                roundList.append(r)
+                if not containsBoth(roundList, r1, r2):
+                    r.append(r1)
+                    r.append(r2)
+                    roundList.append(r)
+                r = []
             if len(roundList) == lengthofRoundList:
                 fillingList = False
 
-            for ro in roundList:
-                c1 = 0
-                c2 = 0
-                for b in self.registry.br:
-                    if isHigherThan(b, ro[0], ro[1]):
-                        c1 += 1
-                    else:
-                        c2 += 1
-                if c1 > c2:
-                    count_register.votes[ro[0]] += 1
-                if c2 > c1:
-                    count_register.votes[ro[1]] += 1
+        for ro in roundList:
+            c1 = 0
+            c2 = 0
+            for b in self.registry.br:
+                if isHigherThan(b, ro[0], ro[1]):
+                    c1 += 1
+                else:
+                    c2 += 1
+            if c1 > c2:
+                count_register.votes[ro[0]] += 1
+            if c2 > c1:
+                count_register.votes[ro[1]] += 1
 
-            biggest_num = 0
-            for v in count_register.votes:
-                if count_register.votes[v] > biggest_num:
-                    biggest_num = count_register.votes[v]
+        biggest_num = 0
+        for v in range(len(count_register.votes)):
+            if count_register.votes[v] > biggest_num:
+                biggest_num = count_register.votes[v]
 
-            count = 0
-            for v in count_register.votes:
-                if count_register.votes[v] == biggest_num:
-                    count += 1
+        count = 0
+        for v in range(len(count_register.votes)):
+            if count_register.votes[v] == biggest_num:
+                count += 1
 
-            if count > 1:
-                print("It's a tie!")
-            else:
-                winner = ""
-                for c in count_register.candidates:
-                    if count_register.votes[c] == biggest_num:
-                        winner = count_register.candidates[c]
+        if count > 1:
+            print("It's a tie!")
+        else:
+            winner = ""
+            for c in range(len(count_register.candidates)):
+                if count_register.votes[c] == biggest_num:
+                    winner = count_register.candidates[c]
 
-                print("Winner is",winner)
+            print("Winner is",winner)
+
+# Who's The Favourite Dude from F.R.I.E.N.D.S.?
+
+candidates = ["Chandler","Joey","Ross"]
+
+r = BallotRegistry()
+
+config = [1,2,3]
+
+b = Ballot(candidates,config)
+
+for i in range(13):
+    r.addBallot(b)
+
+config = [1,3,2]
+
+b = Ballot(candidates,config)
+
+for i in range(7):
+    r.addBallot(b)
+
+config = [2,1,3]
+
+b = Ballot(candidates,config)
+
+for i in range(19):
+    r.addBallot(b)
+
+config = [2,3,1]
+
+b = Ballot(candidates,config)
+
+for i in range(25):
+    r.addBallot(b)
+
+config = [3,1,2]
+
+b = Ballot(candidates,config)
+
+for i in range(29):
+    r.addBallot(b)
+
+config = [3,2,1]
+
+b = Ballot(candidates,config)
+
+for i in range(7):
+    r.addBallot(b)
+
+v = VoteCount(r)
+
+v.plurality()
+v.instantrunoff()
+v.bordacount()
+v.condorcet()
+
+
 
